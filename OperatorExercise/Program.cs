1b32f3d03f728bc35cb03d33c01eb477a1af2805
JakelynnWalker <jakelynnwalker15@icloud.com>
2021-06-10 13:48:14 -0500

    OperatorExercise

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            if (a == 17 && b == 4) ;
+
+            Console.WriteLine($"The answer is {quotient} and the remainder is {remainder}");
+            
+        }
+    }
+}
